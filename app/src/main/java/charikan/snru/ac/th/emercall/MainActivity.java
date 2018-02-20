e901c58eb3c011d3054c5f52f5dcf5e391b33755
charikan <charikan@gmail.com>
2018-02-20 12:06:27 +0700

    add Fraqment Success

--- app/src/main/java/charikan/snru/ac/th/emercall/MainActivity.java
+++ app/src/main/java/charikan/snru/ac/th/emercall/MainActivity.java
@@ -2,6 +2,9 @@ package charikan.snru.ac.th.emercall;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatDialogFragment;
+
+import charikan.snru.ac.th.emercall.fragment.MainFragmenf;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+//        Add Fraqment
+        if (savedInstanceState == null) {
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentMainFragment, new MainFragmenf()).commit();
+
+        }
+
+
+
+    }   // Main Method
+
+}   // Main Class
