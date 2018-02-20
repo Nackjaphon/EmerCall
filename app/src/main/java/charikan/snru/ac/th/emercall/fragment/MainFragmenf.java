34e589bb7f6291fc90ea5186b2b227536ee174c6
charikan <charikan@gmail.com>
2018-02-20 11:31:52 +0700

    Create Fraqment Success

---
+++ app/src/main/java/charikan/snru/ac/th/emercall/fragment/MainFragmenf.java
@@ -0,0 +1,25 @@
+package charikan.snru.ac.th.emercall.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import charikan.snru.ac.th.emercall.R;
+
+/**
+ * Created by Admin on 20/2/2561.
+ */
+
+public class MainFragmenf extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_poilce, container, false);
+        return view;
+    }
+
+}  // Main Class
