abbaa5e6d888375a7cdddee979867d03f68e2b10
charikan <charikan@gmail.com>
2018-02-20 15:25:42 +0700

    add Fraqment Success

--- app/src/main/java/charikan/snru/ac/th/emercall/MainActivity.java
+++ app/src/main/java/charikan/snru/ac/th/emercall/MainActivity.java
@@ -8,6 +8,7 @@ import charikan.snru.ac.th.emercall.fragment.MainFragmenf;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
